0ad4361870d739482e62f752f273fdc5492cc695
Doyoon <qkrehdbs1106@naver.com>
2020-10-14 19:41:46 +0900

    안뇽

---
+++ 미니 프로젝트/crudapp/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .models import Blog
+from .forms import PostForm
+
+def home(request):
+    blogs = Blog.objects
+    return render(request, 'crudapp/home.html', {'blogs' : blogs})
+
+def detail(request, blog_id):
+    blog_detail = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'crudapp/detail.html', {'blog' : blog_detail})
+
+def new(request):
+    return render(request, 'crudapp/new.html')
+
+def postcreate(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('home')
+    else:
+        form = PostForm()
+        return render(request, 'crudapp/new.html', {'form':form})
+
+def edit(request):
+    return render(request, 'crudapp/edit.html')
+
+def postupdate(request, blog_id):
+    post = get_object_or_404(Blog, pk=blog_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('detail', blog_id=post.pk)
+    else:
+        form = PostForm(instance=post)
+        return render(request, 'crudapp/edit.html', {'form':form})
+
+def postdelete(request, blog_id):
+    post = get_object_or_404(Blog, pk=blog_id)
+    post.delete()
+    return redirect('home')
+
